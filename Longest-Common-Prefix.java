cbb1023982f0445ffa84dad69d8a396676fee8e4
Hansaj19 <143512293+Hansaj19@users.noreply.github.com>
2025-08-30 23:58:08 +0530

    [14] [Time Beats: 61.70%] [Memory Beats: 13.34%] - LeetPush

---
+++ Longest-Common-Prefix.java
@@ -0,0 +1,28 @@
+class Solution {
+        public String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
+
+        for (int i = 0; i < strs[0].length(); i++) {
+            char c = strs[0].charAt(i);
+            for (int j = 1; j < strs.length; j++) {
+                if (i >= strs[j].length() || strs[j].charAt(i) != c) {
+                    return strs[0].substring(0, i);
+                }
+            }
+        }
+
+        return strs[0];
+    }
+
+    // For testing
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        String[] strs1 = {"flower", "flow", "flight"};
+        System.out.println(sol.longestCommonPrefix(strs1));  // Output: "fl"
+
+        String[] strs2 = {"dog", "racecar", "car"};
+        System.out.println(sol.longestCommonPrefix(strs2));  // Output: ""
+    }
+}
