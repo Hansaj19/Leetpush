6ed7a9a100676784f889accc5020aa3fefb1031f
Hansaj19 <143512293+Hansaj19@users.noreply.github.com>
2025-10-05 16:53:42 +0530

    [14] [Time Beats: 59.29%] [Memory Beats: 77.84%] - LeetPush

--- Longest-Common-Prefix.java
+++ Longest-Common-Prefix.java
@@ -1,28 +1,27 @@
 class Solution {
-        public String longestCommonPrefix(String[] strs) {
-        if (strs == null || strs.length == 0) {
-            return "";
-        }
+    public String longestCommonPrefix(String[] strs) {
+        int n = strs.length;
+        String res = strs[0];
+        int len = res.length();
+        int mincount = Integer.MAX_VALUE;
 
-        for (int i = 0; i < strs[0].length(); i++) {
-            char c = strs[0].charAt(i);
-            for (int j = 1; j < strs.length; j++) {
-                if (i >= strs[j].length() || strs[j].charAt(i) != c) {
-                    return strs[0].substring(0, i);
+        if (n==0 || n==1) return res;
+        if(len==0) return res;
+
+        for(int i = 1;i<n;i++){
+            int count=0;
+            String temp = strs[i];
+            for(int j=0;j < Math.min(res.length(), temp.length());j++){
+                if(res.charAt(j)==temp.charAt(j)){
+                    count++;
+                }
+                else{
+                    break;
                 }
             }
+            mincount = Math.min(count,mincount);
         }
-
-        return strs[0];
-    }
-
-    // For testing
-    public static void main(String[] args) {
-        Solution sol = new Solution();
-        String[] strs1 = {"flower", "flow", "flight"};
-        System.out.println(sol.longestCommonPrefix(strs1));  // Output: "fl"
-
-        String[] strs2 = {"dog", "racecar", "car"};
-        System.out.println(sol.longestCommonPrefix(strs2));  // Output: ""
+        if(mincount==0|| mincount == Integer.MAX_VALUE) return "";
+        return res.substring(0,mincount);
     }
 }
