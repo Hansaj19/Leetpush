76f5704a05d0d4af73819de9d11deb803636df55
Hansaj19 <143512293+Hansaj19@users.noreply.github.com>
2025-10-09 11:25:49 +0530

    [724] [Time Beats: 100.00%] [Memory Beats: 45.43%] - LeetPush

--- Find-Pivot-Index.java
+++ Find-Pivot-Index.java
@@ -1,21 +1,17 @@
 class Solution {
     public int pivotIndex(int[] nums) {
         int n = nums.length;
-        int prefix[]=new int [n];
-        int suffix[]=new int [n];
-        prefix[0]=0;
-        suffix[n-1]=0;
-        int i=1,j=n-2;
-        while(i<n){
-            prefix[i]=prefix[i-1]+nums[i-1];
-            i++;
-            suffix[j]=suffix[j+1]+nums[j+1];
-            j--;
+        int leftsum=0;
+        int rightsum=0;
+        for(int i=0;i<n;i++){
+            rightsum+=nums[i];
         }
-        for(int k=0;k<n;k++){
-            if(prefix[k]==suffix[k]){
-                return k;
+        for(int i=0;i<n;i++){
+            rightsum-=nums[i];
+            if(rightsum==leftsum){
+                return i;
             }
+            leftsum+=nums[i];
         }
         return -1;
     }
